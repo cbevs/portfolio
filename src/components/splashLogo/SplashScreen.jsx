03bd3603b059a177624a3823ee43395812822964
cbevs <cnbevevino@gmail.com>
2024-08-22 11:26:10 -0400

    fixed bug with hidden div

--- src/components/splashLogo/SplashScreen.jsx
+++ src/components/splashLogo/SplashScreen.jsx
@@ -15,7 +15,7 @@ export const SplashScreen = () => {
 
   return (
     <>
-        <div className="outer-circle">
+        <div className={`outer-circle ${hide}`}>
           
         </div>
         <p className={`greeting-text text-5xl font-extralight ${hide}`}>Hiya!</p>
